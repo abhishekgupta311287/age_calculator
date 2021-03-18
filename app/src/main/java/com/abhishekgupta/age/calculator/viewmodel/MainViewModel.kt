21fd9745ae222f5d1719af705f0c7be09d1fd889
abhishekviu <abhishek.gupta@vuclip.com>
2021-03-18 22:15:46 +0530

    Added MainViewModel implementation

--- app/src/main/java/com/abhishekgupta/age/calculator/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/abhishekgupta/age/calculator/viewmodel/MainViewModel.kt
@@ -1,7 +1,20 @@
 package com.abhishekgupta.age.calculator.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.abhishekgupta.age.calculator.model.UserInfo
+import com.abhishekgupta.age.calculator.repo.IAgeRepo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(private val repo: IAgeRepo) : ViewModel() {
+    val ageLiveData:MutableLiveData<UserInfo> = MutableLiveData()
+
+    fun calculateAge(firstName:String, lastName:String, dob:String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val age = repo.calculateAge(dob)
+            ageLiveData.postValue(UserInfo(firstName, lastName, age))
+        }
+    }
 }
