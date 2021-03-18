d1c344f79e60ac59b7c81f577cd9bcbbdc92cdbf
abhishekviu <abhishek.gupta@vuclip.com>
2021-03-18 22:35:00 +0530

    Added MainViewModel integration with ProfileFragment.kt

--- app/src/main/java/com/abhishekgupta/age/calculator/view/profile/ProfileFragment.kt
+++ app/src/main/java/com/abhishekgupta/age/calculator/view/profile/ProfileFragment.kt
@@ -5,12 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
 import com.abhishekgupta.age.calculator.R
 import com.abhishekgupta.age.calculator.databinding.ProfileFragmentBinding
 import com.abhishekgupta.age.calculator.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.profile_fragment.*
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import java.util.*
 
 class ProfileFragment : Fragment() {
@@ -19,7 +20,7 @@ class ProfileFragment : Fragment() {
         fun newInstance() = ProfileFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel by sharedViewModel<MainViewModel>()
     private var _binding: ProfileFragmentBinding? = null
     private val binding get() = _binding!!
 
@@ -33,8 +34,6 @@ class ProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
 
         binding.dob.setOnClickListener {
             val today = Calendar.getInstance()
@@ -55,8 +54,40 @@ class ProfileFragment : Fragment() {
         }
 
         nextButton.setOnClickListener {
+            val firstName = binding.firstName.text.toString()
+            val lastName = binding.lastName.text.toString()
+            val dob = binding.dob.text.toString()
+            if (isValidInput(firstName, lastName, dob)) {
+                viewModel.calculateAge(
+                    firstName,
+                    lastName,
+                    dob
+                )
+            }
 
         }
     }
+
+    private fun isValidInput(firstName: String, lastName: String, dob: String) =
+        when {
+            firstName.isEmpty() -> {
+                Toast.makeText(requireContext(), getString(R.string.hint_first_name), Toast.LENGTH_LONG)
+                    .show()
+                false
+            }
+            lastName.isEmpty() -> {
+                Toast.makeText(requireContext(), getString(R.string.hint_last_name), Toast.LENGTH_LONG)
+                    .show()
+                false
+            }
+            dob.isEmpty() -> {
+                Toast.makeText(requireContext(), getString(R.string.hint_dob), Toast.LENGTH_LONG)
+                    .show()
+                false
+            }
+            else -> {
+                true
+            }
+        }
 
 }
