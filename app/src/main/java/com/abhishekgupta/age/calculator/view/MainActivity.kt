ca26e106dcd7e03eb434dd988bd1ef55bd82abae
abhishekviu <abhishek.gupta@vuclip.com>
2021-03-18 22:35:52 +0530

    Added MainViewModel integration with MainActivity

--- app/src/main/java/com/abhishekgupta/age/calculator/view/MainActivity.kt
+++ app/src/main/java/com/abhishekgupta/age/calculator/view/MainActivity.kt
@@ -3,9 +3,13 @@ package com.abhishekgupta.age.calculator.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.abhishekgupta.age.calculator.R
+import com.abhishekgupta.age.calculator.view.age.AgeFragment
 import com.abhishekgupta.age.calculator.view.profile.ProfileFragment
+import com.abhishekgupta.age.calculator.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,7 +17,14 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, ProfileFragment.newInstance())
-                .commitNow()
+                .commit()
         }
+
+        viewModel.ageLiveData.observe(this, {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, AgeFragment.newInstance())
+                .addToBackStack("AgeFragment")
+                .commit()
+        })
     }
 }
